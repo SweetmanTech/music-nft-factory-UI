a6fb482689571d3ee206a52705c84b6be89e0542
Sweets Sweetman <sweetmantech@gmail.com>
2022-07-09 17:00:48 -0300

    I add minting functionality.

--- src/components/MintButton/MintButton.js
+++ src/components/MintButton/MintButton.js
@@ -1,9 +1,16 @@
 import { Box, Button } from "@mui/material";
 import { toast } from "react-toastify";
-import { useAccount } from "wagmi";
+import { useAccount, useContract, useSigner } from "wagmi";
+import abi from "./abi.json";
 
 const MintButton = () => {
   const { data: address } = useAccount();
+  const { data: signer } = useSigner();
+  const contract = useContract({
+    addressOrName: "0x1e9A06ec825cf8A7341D344B1f2F2F0386542Ca3",
+    contractInterface: abi,
+    signerOrProvider: signer,
+  });
 
   const handleClick = () => {
     if (!address) {
@@ -13,6 +20,19 @@ const MintButton = () => {
     console.log("ADDRESS", address.address);
 
     console.log("CHECK IF CXY");
+    console.log("CONTRACT", contract);
+    contract
+      .claim()
+      .then(async (tx) => {
+        console.log("TX", tx);
+        const receipt = await tx.wait();
+        console.log("RECEIPT", receipt);
+        toast.success("Minted CXY");
+      })
+      .catch((error) => {
+        toast.error(error?.reason || error.message);
+        console.error(error);
+      });
   };
 
   return (
