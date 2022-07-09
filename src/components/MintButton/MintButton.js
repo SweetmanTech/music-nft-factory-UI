64393caa3f90d4c4188c3e8ebf34c776a25a43a8
Sweets Sweetman <sweetmantech@gmail.com>
2022-07-09 17:03:38 -0300

    I remove dev logging.

--- src/components/MintButton/MintButton.js
+++ src/components/MintButton/MintButton.js
@@ -17,21 +17,15 @@ const MintButton = () => {
       toast.error("Please connect wallet.");
       return;
     }
-    console.log("ADDRESS", address.address);
 
-    console.log("CHECK IF CXY");
-    console.log("CONTRACT", contract);
     contract
       .claim()
       .then(async (tx) => {
-        console.log("TX", tx);
         const receipt = await tx.wait();
-        console.log("RECEIPT", receipt);
         toast.success("Minted CXY");
       })
       .catch((error) => {
         toast.error(error?.reason || error.message);
-        console.error(error);
       });
   };
 
